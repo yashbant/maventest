60c8fc6fbea2c01cb385fd48e57758836415e78b
Yash <yashbantm@gmail.com>
2020-01-23 20:38:26 +0530

    2nd MavenTest updated exe path

--- src/test/java/com/orglearningMaven/HelloWorldTest.java
+++ src/test/java/com/orglearningMaven/HelloWorldTest.java
@@ -1,13 +1,11 @@
 package com.orglearningMaven;
 
-import org.testng.annotations.AfterClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class HelloWorldTest {
-	WebDriver driver;
+	
 @Test 
 	public void Login() {
 	//String projectLocation = System.getProperty("user.dir");
@@ -17,32 +15,4 @@ public class HelloWorldTest {
 	 driver.get("https://google.com");
 }
 
-public void setup() {
-	System.out.println("**");
-	System.out.println("launching chrome browser");
-	 driver = new ChromeDriver();
-	 driver.manage().window().maximize();	
-}
-
-@Test
-public void testPageTitleSampleA() {
-	driver.navigate().to("http://www.google.com");
-	String strPageTitle = driver.getTitle();
-	System.out.println("Page title: - "+strPageTitle);
-	Assert.assertTrue(strPageTitle.equalsIgnoreCase("Google"), "Page title doesn't match");
-}
-@Test
-public void testSampleThree() {
-	System.out.println("Im in test sample three");
-}
-
-@AfterClass
-public void tearDown() {
-	if(driver!=null) {
-		System.out.println("Closing firefox browser");
-		driver.quit();
-	}
-
-
-}
 }
